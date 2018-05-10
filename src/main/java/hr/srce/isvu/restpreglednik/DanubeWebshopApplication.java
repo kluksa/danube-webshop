a834a3420d6259601246d84dc7bf008364468fc6
Lukša Kraljević <lkraljevic@srce.hr>
2018-05-10 08:28:09 +0200

    Pocetak

---
+++ src/main/java/hr/srce/isvu/restpreglednik/DanubeWebshopApplication.java
@@ -0,0 +1,12 @@
+package hr.srce.isvu.restpreglednik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanubeWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanubeWebshopApplication.class, args);
+	}
+}
---
+++ src/test/java/hr/srce/isvu/restpreglednik/DanubeWebshopApplicationTests.java
@@ -0,0 +1,16 @@
+package hr.srce.isvu.restpreglednik;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DanubeWebshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
