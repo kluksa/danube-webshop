7c7db3d281539ce64f1c736462ade4312d17ef97
Lukša Kraljević <lkraljevic@srce.hr>
2018-05-11 13:51:02 +0200

    Dodao authentikaciju, ali nisam isprobao

--- src/main/java/com/ag04/danubewebshop/config/SecurityConfiguration.java
+++ src/main/java/com/ag04/danubewebshop/config/SecurityConfiguration.java
@@ -26,4 +26,33 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       http.headers().frameOptions().disable();
    }
 
+//   http://www.mkyong.com/spring-security/spring-security-hibernate-annotation-example/
+//   @Autowired
+//   @Qualifier("userDetailsService")
+//   UserDetailsService userDetailsService;
+//
+//   @Autowired
+//   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//      auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//   }
+//
+//   @Override
+//   protected void configure(HttpSecurity http) throws Exception {
+//
+//       http.authorizeRequests().antMatchers("/admin/**")
+//      .access("hasRole('ROLE_ADMIN')").and().formLogin()
+//      .loginPage("/login").failureUrl("/login?error")
+//      .usernameParameter("username")
+//      .passwordParameter("password")
+//      .and().logout().logoutSuccessUrl("/login?logout")
+//      .and().csrf()
+//      .and().exceptionHandling().accessDeniedPage("/403");
+//   }
+//   
+//   @Bean
+//   public PasswordEncoder passwordEncoder(){
+//      PasswordEncoder encoder = new BCryptPasswordEncoder();
+//      return encoder;
+//   }
+   
 }
