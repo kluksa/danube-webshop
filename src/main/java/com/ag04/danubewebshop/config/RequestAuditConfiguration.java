ad8f9a039d932468cae922bbb68c7e05bf7e6bad
Luksa Kraljevic <kraljevic@cirus.dhz.hr>
2018-05-15 00:04:53 +0200

    Imam i loggere.

--- src/main/java/com/ag04/danubewebshop/config/RequestAuditConfiguration.java
+++ src/main/java/com/ag04/danubewebshop/config/RequestAuditConfiguration.java
@@ -3,39 +3,24 @@
  */
 package com.ag04.danubewebshop.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.ag04.danubewebshop.web.WebRequestInterceptor;
 
 /**
  * @author Lukša Kraljević, Srce
  */
 @Configuration
-public class RequestAuditConfiguration implements WebMvcConfigurer {
-   
-   @Autowired
-   private WebRequestInterceptor webRequestInterceptor;
-   
-   @Bean
-   public CommonsRequestLoggingFilter logFilter() {
-      CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
-      filter.setIncludeQueryString(true);
-      filter.setIncludePayload(true);
-      filter.setMaxPayloadLength(10000);
-      filter.setIncludeHeaders(true);
-      filter.setAfterMessagePrefix("REQUEST DATA : ");
-      return filter;
-   }
-   
-   @Override
-   public void addInterceptors(InterceptorRegistry registry) {
-       registry.addInterceptor(webRequestInterceptor)
-         .addPathPatterns("/**/");
-   }
+public class RequestAuditConfiguration {
 
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setMaxPayloadLength(10000);
+		filter.setIncludeHeaders(false);
+		filter.setAfterMessagePrefix("REQUEST DATA : ");
+		return filter;
+	}
 }
